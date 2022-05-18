12ed94be31b6e429bfbdf76e7855ce8bfc47323a
Carlos Kohaute <97993704+CarlosKohaute@users.noreply.github.com>
2022-05-17 22:49:38 -0300

    Commit fetch

---
+++ assets/script.js
@@ -0,0 +1,24 @@
+const baseURL = 'http://localhost:3000';
+
+async function findAllCells() {
+  const response = await fetch(`${baseURL}/cells`);
+
+  const cells = await response.json();
+
+  cells.forEach(function (cell) {
+    document.querySelector('#cellList').insertAdjacentHTML(
+      'beforeend',
+      `
+    <div class="cellListItem">
+            <div>
+              <div class="cellListItem_name">${cell.name}</div>
+              <div class="cellListItem_price">R$ ${cell.price}</div>
+              <div class="cellListItem_description">${cell.description}</div>
+            </div>
+      <img class="cellListItem_photo" src="${cell.photo}" alt="Celular ${cell.name}" />
+  </div>`
+    );
+  });
+}
+
+findAllCells();
